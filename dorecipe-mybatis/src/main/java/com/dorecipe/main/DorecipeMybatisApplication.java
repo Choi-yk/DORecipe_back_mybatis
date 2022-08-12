e39dcf97e802a7bfe3e72d166b1271ab5b1356ae
choiyk <weareyk@gmail.com>
2022-08-12 13:20:17 +0900

    push test

---
+++ dorecipe-mybatis/src/main/java/com/dorecipe/main/DorecipeMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.dorecipe.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DorecipeMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DorecipeMybatisApplication.class, args);
+	}
+
+}
