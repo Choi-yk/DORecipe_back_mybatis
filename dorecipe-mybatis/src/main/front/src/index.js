b201d6ae16ffd05fb499993e41ef1f603183cc30
Pyotato <pyolovely01@gmail.com>
2022-08-25 11:47:57 +0900

    not using redux yet

--- dorecipe-mybatis/src/main/front/src/index.js
+++ dorecipe-mybatis/src/main/front/src/index.js
@@ -1,11 +1,9 @@
-import {React, Provider} from "react";
+import { React, Provider } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import store from "./store";
-
-
 
 // import * as serviceWorker from "react";
 import SwaggerUI from "swagger-ui-react";
@@ -13,14 +11,12 @@ import "swagger-ui-react/swagger-ui.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<>
-
-
-   <Provider store={store}>
+  <>
+    {/* <Provider store={store}> */}
     <BrowserRouter>
-    {/* <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" /> */}
+      {/* <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" /> */}
       <App />
     </BrowserRouter>
-   </Provider>
-</>
+    {/* </Provider> */}
+  </>
 );
