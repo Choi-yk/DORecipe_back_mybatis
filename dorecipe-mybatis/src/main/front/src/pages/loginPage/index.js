be50626e2930cd48f53302bb1d55c34a6802368d
choiyk <weareyk@gmail.com>
2022-08-31 22:14:36 +0900

    loginPage react

--- dorecipe-mybatis/src/main/front/src/pages/loginPage/index.js
+++ dorecipe-mybatis/src/main/front/src/pages/loginPage/index.js
@@ -3,6 +3,8 @@ import { useState, useCallback, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import MainLayout from "../../layout/mainLayOut";
+import { MainLogo } from "../../components/_common/mainLogo";
+import JoinMemberPage from "../joinMemberPage";
 
 const LoginPage = () => {
 	
@@ -31,44 +33,42 @@ const LoginPage = () => {
 	return (
 		<>
 		<MainLayout>
-		<div class="loginSection">
-			{/* 로고 */}
-			<div class="loginWrap">
-				<div class="logoWrap">
-					<Link to={"/"}>
-						<img class="logo" src="../../../public/img/doRecipeLogo.gif" />
-					</Link>
-				</div>
-				{/*  아이디, 비밀번호 입련란 */}
-				<div class="formWrap">
-					<form action="#" method="get">
-						<input
-							name="member_id"
-							class="idInput"
-							required
-							type="text"
-							placeholder="아이디"
-						/>
-						<input
-							class="pwdInput"
-							required
-							type="password"
-							placeholder="비밀번호"
-						/>
-						<button type="submit" name="loginBtn" onclick="alertBlanks()">
-							로그인
-						</button>
-					</form>
-				</div>
+			<div class="loginSection">
+				{/* 로고 */}
+				<div class="loginWrap">
+					<div class="logoWrap">
+						<MainLogo />
+					</div>
+					{/*  아이디, 비밀번호 입력란 */}
+					<div class="formWrap">
+						<form action="#" method="get">
+							<input
+								name="member_id"
+								class="idInput"
+								required
+								type="text"
+								placeholder="아이디"
+							/>
+							<input
+								class="pwdInput"
+								required
+								type="password"
+								placeholder="비밀번호"
+							/>
+							<button type="submit" name="loginBtn" onclick="alertBlanks()">
+								로그인
+							</button>
+						</form>
+					</div>
 
-				{/* 아이디, 비밀번호 찾기, 회원가입 */}
-				<div class="linkWraps">
-					<a href="#">아이디 찾기</a> | <a href="#">비밀번호 찾기</a> |
-					<a href="./joinmember.html">회원가입</a>
+					{/* 아이디, 비밀번호 찾기, 회원가입 */}
+					<div class="linkWraps">
+					<Link to={"#"}>아이디 찾기</Link> | <Link to={"#"}>비밀번호 찾기</Link> | <Link to={"/join"}>회원가입</Link>
+
+					</div>
 				</div>
 			</div>
-		</div>
-		</MainLayout>
+			</MainLayout>
 		</>
 	);
 };
